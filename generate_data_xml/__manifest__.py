87c8a3a65ff4c12ed45a8ac8139da4ba4618c6b2
AngioC <cotardoangelo@gmail.com>
2024-09-10 13:01:28 +0200

    manifest

--- generate_data_xml/__manifest__.py
+++ generate_data_xml/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    "name": "generate_data_xml",
+    "name": "XML data file generator",
     "summary": "Generate XLM data",
     "version": "14.0.0.0.1",
     "development_status": "Alpha",
